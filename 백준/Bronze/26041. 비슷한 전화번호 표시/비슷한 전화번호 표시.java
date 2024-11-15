ee7b67d662b84e5275de060743a32349d73c1a91
m._jj <136244456+Park-MJeong@users.noreply.github.com>
2024-11-15 14:09:48 +0900

    [Bronze II] Title: 비슷한 전화번호 표시, Time: 268 ms, Memory: 27376 KB -BaekjoonHub

--- 백준/Bronze/26041. 비슷한 전화번호 표시/비슷한 전화번호 표시.java
+++ 백준/Bronze/26041. 비슷한 전화번호 표시/비슷한 전화번호 표시.java
@@ -1,26 +1,25 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 /*
-* B를 접두사로 가지는 A전화번호 찾기
-* A와 B가 완전히 똑같으면 안된다.
-* A의 길이는 B보다 무조건 길어야한다.*/
+startsWith(b) : b로시작 하는지 여부
+* */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String [] input =br.readLine().split(" ");
-        String B =br.readLine() ;
-        int cnt=0;
-        for (String input_A : input){
-            int tmp =0;
-            if(!input_A.equals(B) && B.length()<input_A.length() ){ //완전히 똑같은 수 제외,B보다 작은 길이 제외
-                for(int j=0;j<B.length();j++) {
-                    if(input_A.charAt(j) == B.charAt(j)){
-                        tmp++;
-                    }
-                }
+        String a = br.readLine();
+        String b = br.readLine();
+
+        int cnt = 0;
+        String [] str = a.split(" "); 
+        for(String s : str){
+            if(!s.equals(b) && s.startsWith(b)){
+                cnt++;
             }
-           cnt = tmp == B.length() ? cnt+1 : cnt; //길이가 같으면 B를접두사로 가지는것
         }
         System.out.println(cnt);
-    }
 
+    }
 }
