14915c7ff0a590046471f43f16283a3c75fecb4b
m._jj <136244456+Park-MJeong@users.noreply.github.com>
2024-11-29 13:14:24 +0900

    [Silver III] Title: 크리스마스 선물, Time: 360 ms, Memory: 40016 KB -BaekjoonHub

---
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+* 입력
+방문횟수 n
+a개의 숫자
+=> 각 숫자들은 선물의 개수 선물의 가치(단,0이라면 방문한것)
+*
+* 출력
+a가 0일때마다 선물 가치 출력
+선물이 없다먼 -1출력
+* */
+
+/* 문제해결
+가치가 큰 선물 하나의 값을 꺼내는것이므로 최대힙
+0이면 힙에서 꺼낸 값 출력, 만약 힙이 비어있다면 -1출력
+* */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String[] arr = br.readLine().split(" ");
+            if(arr[0].equals("0")){
+                if(pq.isEmpty()) sb.append(-1).append("\n");
+                else sb.append(pq.poll()).append("\n");
+            }else {
+                for(int j = 1; j < arr.length; j++) {
+                    pq.offer(Integer.parseInt(arr[j]));
+                }
+            }
+        }
+        System.out.println(sb);
+
+
+    }
+}
