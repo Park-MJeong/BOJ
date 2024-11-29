07d0ca6197a0e9f35551c946488d3d7bfc09ff74
m._jj <136244456+Park-MJeong@users.noreply.github.com>
2024-11-29 11:48:51 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14172 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,50 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+* 입력
+문자열 길이 L
+영문 소문자로만 이루어진 문자열
+*
+* 출력
+문자열을 해시값 정수로 계산하여 출력
+* */
+
+/* 문제해결
+a=1,b=2,c=3,d=4 ... z=26
+처음들어온수는 1, 두번째는 31, 세번째는 31^2 ... 최대 5글자이므로 마지막은 31^5
+
+우선, 테이블에 각각 알파벳의 숫자를 저장한다ㅏ.
+a는 아스키 코드로 97번
+
+문자열의 위치값은 31의 n승이 될것임
+해당 문자열은 테이블에서 해당 숫자를 찾아 두개를 곱한다.
+문자열끼리의 값은 더해준다.
+
+* */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Hashtable<Character,Integer> table = new Hashtable<>() ;
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        int sum = 0;
+
+        for(int i=1 ; i<=26 ; i++){
+            table.put((char)(96+i),i);
+        }
+        for(int i=0 ; i<n ; i++){
+            sum+= table.get(s.charAt(i))* Math.pow(31, i);
+        }
+        System.out.println(sum);
+
+    }
+}
+
+
+
