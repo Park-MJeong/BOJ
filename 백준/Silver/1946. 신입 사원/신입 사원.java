cc3c697ef3d348b18b0fd4ee058be88476673bc0
m._jj <136244456+Park-MJeong@users.noreply.github.com>
2025-02-14 01:12:28 +0900

    [Silver I] Title: 신입 사원, Time: 2136 ms, Memory: 313828 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,76 +1,46 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Map;
+import java.util.TreeMap;
 
-/*
-* 입력
-테스트 케이스 t
-지원자 수 n
-n개의 줄 각각 서류성적,면접성적의 순위
-* 출력
-한줄에 하나씩 선별할 수 있는 신입사원 최대인원수 출력
-* */
-
-/* 문제해결
-두 성적 순위 모두 1위~n위까지 동석차없음
-서류순위를 오름 차순 정렬한다.
-서류순위가 1위인 면접자의 면접순위A를 구한다. ++1
-면접1순위~ A순위까지의 서류순위를 확인
-
-서류 n순위가  A보다 작고, 본인보다 서류순위가 작은 사람보다 면접순위가 더 작아야함
-그러니. 이전 서류의 면접순위보다 작아야함 ++1
-면접1순위를 만나면 거기까지 cnt 를 더하고 출력
-왜냐면 서류1~n순위
-면접n순위~1순위 까지만
-적어도 하나가 다른 지원자보다 떨어지지않는 자에 포함되기 때문에
-* */
-public class Main {
-
-
+public class Main{
+    static StringBuilder sb = new StringBuilder();
+    static Map<Integer,Integer> map = new TreeMap<>();
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<Integer, Integer> map = new TreeMap<>();
-        StringBuilder sb = new StringBuilder();
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 
-
-        int t = Integer.parseInt(br.readLine());
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            for (int j = 0; j < n; j++) {
-                String [] s = br.readLine().split(" ");
-                map.put(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-
+        int T =Integer.parseInt(br.readLine());
+        for(int i=0;i<T;i++){
+            int N =Integer.parseInt(br.readLine());
+            for(int j=0;j<N;j++){
+                String[] str =br.readLine().split(" ");
+                int a = Integer.parseInt(str[0]);
+                int b = Integer.parseInt(str[1]);
+                map.put(a,b);
             }
-            sunwi(map,n,sb);
+            sunwi(N);
         }
         System.out.println(sb);
 
     }
-
-    static void sunwi(Map<Integer,Integer> map,int n,StringBuilder sb){
-        int k = map.get(1); // 서류 1순위의 면접 순위
-        int result = 1; // 서류 1순위는 항상 선발
-        int minRank = k; // 현재까지의 최소 면접 순위
-
-        for (int i = 2; i <= n; i++) {
-            int temp = map.get(i);//서류2순위부터 면접순위 체크
-
-            // 면접 순위가 1이라면 즉시 종료
-            if (temp == 1) {
+    static void sunwi(int n){
+        int minRank = map.get(1);
+        int result = 1;
+        for(int i=2;i<=n;i++){
+            int k = map.get(i);
+            if(k==1){
                 result++;
                 sb.append(result).append("\n");
                 return;
             }
-
-            // 현재 지원자의 면접 순위가 기준에 부합하면 선발
-            if (temp < minRank) {
+            if(k<minRank){
                 result++;
-                minRank = temp; // 최소 면접 순위 갱신
+                minRank = k;
             }
         }
-        // 최종 결과 추가
         sb.append(result).append("\n");
 
     }
-}
+        }
